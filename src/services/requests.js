39e6ffe3860da6db7f32356f676eb9ed7e9fd813
bberos <bberos.171@gmail.com>
2022-02-09 13:59:09 -0300

    feat(request): services

---
+++ src/services/requests.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+export const getDetailedCoinData = async (coinId) => {
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false`
+    );
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getCoinMarketChart = async (coinId) => {
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=1&interval=hourly`
+    );
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getMarketData = async (page = 1) => {
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}&sparkline=false&price_change_percentage=24h`
+    );
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getFavouritesCoins = async (page = 1, coinIds) => {
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinIds}&order=market_cap_desc&per_page=50&page=${page}&sparkline=false&price_change_percentage=24h`
+    );
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
