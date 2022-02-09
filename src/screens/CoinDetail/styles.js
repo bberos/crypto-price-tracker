d9ba77a43d6757236d033bb6113a2d853989b7c0
bberos <bberos.171@gmail.com>
2022-02-09 13:58:27 -0300

    styles(CoinDetailScreen)

---
+++ src/screens/CoinDetail/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    width: 130,
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+    padding: 10,
+    fontSize: 16,
+    color: "white",
+  },
+});
+export default styles;
