f2bc757b1849f04ff03a482847d88d46e53168fe
bberos <bberos.171@gmail.com>
2022-02-09 13:58:44 -0300

    feat(FavouriteScreen)

---
+++ src/screens/FavouritesScreen/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { FlatList, RefreshControl } from "react-native";
+import { useFavourites } from "./../../context/FavouritesProvider";
+import CoinCard from "./../../components/CoinCard";
+import { getFavouritesCoins } from "./../../services/requests";
+export default function index() {
+  const { favouritesCoinIds } = useFavourites();
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const convertCoinIds = () => favouritesCoinIds.join("%2C");
+
+  const fetchFavouritesCoins = async () => {
+    if (isLoading) {
+      return;
+    }
+    setIsLoading(true);
+    const favouritesCoinsData = await getFavouritesCoins(1, convertCoinIds());
+    setCoins(favouritesCoinsData);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchFavouritesCoins();
+  }, [favouritesCoinIds]);
+
+  return (
+    <FlatList
+      data={coins}
+      renderItem={({ item }) => <CoinCard marketCoin={item} />}
+      refreshControl={
+        <RefreshControl
+          refreshing={isLoading}
+          tintColor="white"
+          onRefresh={fetchFavouritesCoins}
+        />
+      }
+    />
+  );
+}
