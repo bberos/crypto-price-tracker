48e165e2f47fcfc96422e995414ff1e011143e4d
bberos <bberos.171@gmail.com>
2022-02-10 16:14:30 -0300

    feat(navigation): new stack.screen "addnewasset"

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -2,16 +2,36 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CoinDetail from "./../screens/CoinDetail";
 import BottomTabNavigator from "./BottomTabNavigator";
+import AddNewAssetScreen from "../screens/AddNewAsset";
 const Stack = createNativeStackNavigator();
 
 function Navigation() {
   return (
-    <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="Root"
-    >
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="CoinDetail" component={CoinDetail} />
+    <Stack.Navigator initialRouteName="Root">
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="CoinDetail"
+        component={CoinDetail}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="AddNewAssetScreen"
+        component={AddNewAssetScreen}
+        options={{
+          title: "Add New Asset",
+          headerStyle: {
+            backgroundColor: "#121212",
+          },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
