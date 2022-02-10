e2511142ca34ce23fd5bd0a35b944044fa421c66
bberos <bberos.171@gmail.com>
2022-02-10 16:15:16 -0300

    feat(PortfolioScreen): new screen

---
+++ src/screens/Portfolio/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import PortfolioList from "../../components/PortfolioList";
+
+export default function PortfolioScreen() {
+  return (
+    <View>
+      <PortfolioList />
+    </View>
+  );
+}
