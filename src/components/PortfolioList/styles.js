dc41a92b1e247c807bd6a313076107056081ed8b
bberos <bberos.171@gmail.com>
2022-02-10 16:13:38 -0300

    styles(PortfolioList)

---
+++ src/components/PortfolioList/styles.js
@@ -0,0 +1,62 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  balanceContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 5,
+    marginHorizontal: 10,
+  },
+  currentBalance: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 15,
+  },
+  currentBalanceValue: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 40,
+    letterSpacing: 1,
+  },
+  valueChange: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  percentageChange: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 17,
+  },
+  priceChangePercentageContainer: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    paddingHorizontal: 5,
+    borderRadius: 5,
+    backgroundColor: "#16c784",
+  },
+  assetsTitle: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 23,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    backgroundColor: "#4169E1",
+    padding: 10,
+    alignItems: "center",
+    marginVertical: 25,
+    marginHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "600",
+  },
+});
+
+export default styles;
