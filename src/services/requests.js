8313bd12f03ab4a3eb585f2106eb60fb63b3a752
bberos <bberos.171@gmail.com>
2022-02-11 12:23:18 -0300

    feat(request): new servcice getAllCoins

--- src/services/requests.js
+++ src/services/requests.js
@@ -43,3 +43,14 @@ export const getFavouritesCoins = async (page = 1, coinIds) => {
     console.log(e);
   }
 };
+
+export const getAllCoins = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/list?include_platform=false`
+    );
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
