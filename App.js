7133f4391aa557c7db08a49f3bad4f65b599d2e8
bberos <bberos.171@gmail.com>
2022-02-14 19:32:13 -0300

    feat(App): add custom font DriodSans and Inter Google font

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation";
 import FavouritesProvider from "./src/context/FavouritesProvider";
 import { RecoilRoot } from "recoil";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    DriodSans: require("./assets/fonts/DroidSans.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size={"large"} />;
+  }
   return (
     <NavigationContainer
       theme={{
