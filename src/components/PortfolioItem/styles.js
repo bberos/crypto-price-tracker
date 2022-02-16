ec066b9ea73ce10b8a1c3080da491e209d23351a
bberos <bberos.171@gmail.com>
2022-02-16 19:45:38 -0300

    styles(PorfolioItem): backgroundColor for react-swipeable

--- src/components/PortfolioItem/styles.js
+++ src/components/PortfolioItem/styles.js
@@ -4,6 +4,8 @@ const styles = StyleSheet.create({
   itemContainer: {
     flexDirection: "row",
     padding: 15,
+    //this background has to be the same of background app because swipeable-view-list
+    backgroundColor: "#121212",
   },
   coinName: {
     color: "white",
