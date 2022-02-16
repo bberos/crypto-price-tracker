2d732131379aefd6827b84c3d8a5f52562c5c579
bberos <bberos.171@gmail.com>
2022-02-16 17:26:32 -0300

    feat(services): getCoinMarketChart new param selectedRange for filter chart

--- src/services/requests.js
+++ src/services/requests.js
@@ -11,10 +11,10 @@ export const getDetailedCoinData = async (coinId) => {
   }
 };
 
-export const getCoinMarketChart = async (coinId) => {
+export const getCoinMarketChart = async (coinId, selectedRange) => {
   try {
     const response = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=1&interval=hourly`
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${selectedRange}}&interval=hourly`
     );
     return response.data;
   } catch (e) {
