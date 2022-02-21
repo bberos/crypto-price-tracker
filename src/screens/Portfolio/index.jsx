085145e9083049c7a56d70bd3425ea5d095334c3
bberos <bberos.171@gmail.com>
2022-02-21 13:50:47 -0300

    feat(Portfolio): add ActivityIndicator in Suspense

--- src/screens/Portfolio/index.jsx
+++ src/screens/Portfolio/index.jsx
@@ -1,11 +1,17 @@
 import React, { Suspense } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import PortfolioList from "../../components/PortfolioList";
 
 export default function PortfolioScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <Suspense fallback={<Text style={{ color: "white" }}>Loading</Text>}>
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Suspense
+        fallback={
+          <Text style={{ color: "white", alignSelf: "center" }}>
+            <ActivityIndicator />
+          </Text>
+        }
+      >
         <PortfolioList />
       </Suspense>
     </View>
